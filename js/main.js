f1c82a88018494eb14c6085fad2ae93e18e97dcb
Manisangh Sharma <78167300+manisanghsharma@users.noreply.github.com>
2023-09-09 19:21:20 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@
     });
 
 
-    
+    // Skills
     $('.skill').waypoint(function () {
         $('.progress .progress-bar').each(function () {
             $(this).css("width", $(this).attr("aria-valuenow") + '%');
@@ -51,7 +51,27 @@
     });
 
 
-    
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 25,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            992:{
+                items:2
+            }
+        }
+    });
 
 
     // Portfolio isotope and filter
